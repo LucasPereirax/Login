cb6430c1a260bf0809cb005a9a0a87e9fe8fced9
Lucas Pereira <lucaspereiraoliveira10@hotmail.com>
2026-03-20 17:02:54 -0300

    --ajustes
    Realizados alguns ajustes no arquivo JS para iniciar algumas validações

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,23 @@
-const inputs = document.querySelectorAll('.code-input');
 
-inputs.forEach((input, index) => {
+
+
+//pular conforme o usuário digita o código
+const inputs_code = document.querySelectorAll('.code-input');
+
+inputs_code.forEach((input, index) => {
     input.addEventListener('input', () => {
-        if (input.value.length === 1 && index < inputs.length - 1) {
-            inputs[index + 1].focus(); 
+        if (!isNaN(input.value) && input.value !== ' ') {
+            if (input.value.length === 1 && index < inputs_code.length - 1) {
+                inputs_code[index + 1].focus(); 
+            }
+        }else {
+            input.value = ''; 
         }
     });
 
     input.addEventListener('keydown', (e) => {
         if (e.key === 'Backspace' && input.value === '' && index > 0) {
-            inputs[index - 1].focus(); 
+            inputs_code[index - 1].focus(); 
         }
     });
 });
